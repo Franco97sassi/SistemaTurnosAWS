e68dd480df7b8f91a313ea0d34b30b4f008dc140
fms1997 <franco.sassi@brio.com.ar>
2026-06-26 14:18:24 -0300

    Habilitar CORS para frontend

--- SistemaTurnosAWS/backend/app/main.py
+++ SistemaTurnosAWS/backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.routers import clientes, profesionales, estados, turnos
@@ -10,6 +11,17 @@ app = FastAPI(
     title="Sistema de Turnos AWS",
     description="API simple para estudiar Cloud y DevOps con AWS",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(clientes.router)
